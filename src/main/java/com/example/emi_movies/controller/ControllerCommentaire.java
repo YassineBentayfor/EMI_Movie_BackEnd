39058e44125b05eb0403d984b92a0916111eae72
Yassine Bentayfor <yassine.bentayfor2015@gmail.com>
2024-01-02 23:17:01 +0100

    final

--- src/main/java/com/example/emi_movies/controller/ControllerCommentaire.java
+++ src/main/java/com/example/emi_movies/controller/ControllerCommentaire.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/Commentaire")
 public class ControllerCommentaire {
     private final ServiceCommentaire serviceCommentaire;
